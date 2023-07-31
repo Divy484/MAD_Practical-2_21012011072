1ca3cd35f573068e4d8474552bc2d922378a2315
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-07-31 08:06:53 +0530

    Practical 2

--- app/src/main/java/com/example/mad_practical_2_21012011072/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_21012011072/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -42,9 +44,13 @@ class MainActivity : AppCompatActivity() {
         super.onRestart()
         showMsg("onRestart method is called.")
     }
-    
+
     fun showMsg(message: String) {
         Log.i(TAG, message)
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val constraint : ConstraintLayout? = findViewById(R.id.mainConstraint)
+        if (constraint != null){
+            Snackbar.make(constraint,message,Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
